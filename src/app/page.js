788a486e78c5d373813d9478a39d36ffcae9b46e
glsndroj <galsandooorj@gmail.com>
2025-09-09 09:20:00 +0800

    todolist

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,9 @@
-"use client";
-import { useState } from "react";
+import { ToDoList } from "../Components/ToDoList";
 
 export default function Home() {
-  const [number, setNumber] = useState(1);
-  const Increase = () => {
-    setNumber(number + 1);
-  };
-
-  const Decrease = () => {
-    setNumber(number - 1);
-  };
   return (
-    <div className=" flex gap-10 flex-col m-50">
-      <p className="text-4xl">Hello - {number}</p>
-      <button onClick={Increase} className="w-[200px] h-[60px] bg-amber-700">
-        Increase
-      </button>
-      <button
-        onClick={Decrease}
-        className="w-[200px] h-[60px] bg-amber-700"
-      >
-        Decrease
-      </button>
+    <div className="Container flex justify-center mt-[60px]">
+      <ToDoList />
     </div>
   );
 }
